4a448806242a54bf0d712c0e62867439107d1673
Timmetie01 <tnmoosterom@hotmail.com>
2026-02-26 12:31:11 +0100

    window size

--- test.py
+++ test.py
@@ -5,5 +5,5 @@ import filters
 data = data_import.get_velocity_test_data()
 print(data)
 
-window_size = 251
+window_size = 51
 graphing.test_filter(filters.moving_averages(data, window_size, extension_type='none'), data, filter_shift=(window_size-1)/2)
