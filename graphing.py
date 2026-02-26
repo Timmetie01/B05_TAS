9be3a97362d31bd2295ec39deb0f66a5d27c3057
Timmetie01 <tnmoosterom@hotmail.com>
2026-02-26 11:32:03 +0100

    Graphing filters to analyze accuracy possible now

--- graphing.py
+++ graphing.py
@@ -1,11 +1,36 @@
-def test_filter(filtered_data, input_data, truth=None, showplot=True):
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def test_filter(filtered_data, input_data, discrete_points=True, truth=None, showplot=True):
     '''
     Graphs the filtered data, input data and if present truth
     
     :param filtered_data: 1 or 2d array of the filtered data. Plotted along axis 1
     :param input_data: 1 or 2d array of the original data points. Plotted along axis 1    
     '''
+    if filtered_data.ndim == 1:
+        filtered_data = np.transpose(np.array([filtered_data]))
+        input_data = np.transpose(np.array([input_data]))
+        truth = np.transpose(np.array([truth]))
+
+    if filtered_data.ndim > 2 or input_data.ndim != filtered_data.ndim:
+        print('shouldnt input higher than 2 dimensional array, and array dimensions should be the same for filtered and input data.')
+        quit()
 
 
-
+    for i in range(len(filtered_data[0,:])):
+        print(len(filtered_data[:,i]))
+        plt.plot(np.arange(0, len(filtered_data[:,i]), 1), filtered_data[:,i], label='Filtered data', color='darkblue')
+        if discrete_points:
+            plt.scatter(np.arange(0, len(input_data[:,i]), 1), input_data[:,i], label='Original data', color='firebrick')
+        else:
+            plt.plot(np.arange(0, len(input_data[:,i]), 1), input_data[:,i], label='Original data', color='firebrick')
     
+    if showplot:
+        plt.title('Filtered vs unfiltered data')
+        plt.legend()
+        plt.show()
+
+
+test_filter(np.array([0,0.1, 0.19, 0.3, 0.4]), np.array([0.1, 0.11, 0.19, 0.32, 0.37]))
