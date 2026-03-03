56ca138d866c961a772e3025ac3cbfed8f22650e
Timmetie01 <tnmoosterom@hotmail.com>
2026-03-03 13:06:08 +0100

    Additional testing

--- test.py
+++ test.py
@@ -5,4 +5,4 @@ import classes
 
 data = classes.Data('test')
 
-data.plot_ma_filter(51,derivative=0,  extension_type='mirror')
+data.plot_ma_filter(51,derivative=1,  extension_type='mirror', discrete_points=False)
