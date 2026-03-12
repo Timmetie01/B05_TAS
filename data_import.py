01012fa52d787335b7252333ff3d4459b8351402
Timmetie01 <tnmoosterom@hotmail.com>
2026-03-12 11:49:31 +0100

    Delete dataset

--- data_import.py
+++ data_import.py
@@ -3,6 +3,7 @@ import numpy as np
 
 def get_velocity_test_data():
     #For reference: importing 250k test datapoints in a 1d array takes approximately 0.0364 seconds.
+    raise NotImplementedError
     data = pd.read_csv("Velocitydatatest.csv")
     return pd.DataFrame.to_numpy(data)
 
