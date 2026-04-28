cf5baa60cfc605f29d90da614b44aacd74edda69
emcia112006-coder <emcia112006@gmail.com>
2026-04-28 12:27:33 +0200

    update

--- center_points_analysis.py
+++ center_points_analysis.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-# Your cyan dot coordinates
 cyan_dots1 = np.array([[x1, y1, z1], [x2, y2, z2], ...])  
 cyan_dots2 = np.array([[x1, y1, z1], [x2, y2, z2], ...])
 cyan_dots3 = np.array([[x1, y1, z1], [x2, y2, z2], ...])
@@ -8,19 +7,15 @@ cyan_dots4 = np.array([[x1, y1, z1], [x2, y2, z2], ...])
 
 all_cyan_dots = [cyan_dots1, cyan_dots2, cyan_dots3, cyan_dots4]
 
-# Theoretical center (orange dot)
 ref_center = np.array([x_ref, y_ref, z_ref])  
 
 def error(cyan_dots, ref_center=ref_center):
     # Euclidean distances
     distances = np.linalg.norm(cyan_dots - ref_center, axis=1)
-
     # Average displacement
     mean_displacement = np.mean(distances)
-
     # Standard deviation
     std_displacement = np.std(distances)
-    
     return mean_displacement, std_displacement
 
 for i, cyan_dots in enumerate(all_cyan_dots):
