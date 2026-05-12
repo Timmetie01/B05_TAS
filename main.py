ddb0f9150cbfbbbf34d0dfceb5b04fcb12446aba
agathe7715 <agathe.e.blain@gmail.com>
2026-05-12 11:19:32 +0200

    added my function

---
+++ main.py
@@ -0,0 +1,50 @@
+import classes
+import calculations
+import matplotlib.pyplot as plt
+import numpy as np
+
+take_1 = classes.Data('take_001')
+take_2 = classes.Data('take_002')
+take_3 = classes.Data('take_003')
+take_4 = classes.Data('take_004')
+take_5 = classes.Data('take_005')
+
+take_1.plot_maneuver_duration(-20, 20)
+take_1.plot_operations('arm_position', ('ma_filter_5', 'derivative_1'), (False, False, True), color='darkblue', label='arm_Z', showplot=False)
+take_1.plot_operations('arm_position', ('ma_filter_5', 'derivative_1'), (False, True, False), color='darkorange', label='arm_Y', showplot=False)
+take_1.plot_operations('arm_position', ('ma_filter_5', 'derivative_1'), (True, False, False), color='purple', label='arm_X', showplot=False)
+plt.legend(fontsize=20)
+plt.xlabel('Time [s]', fontsize=15)
+plt.ylabel('Velocity [mm/s]', fontsize=15)
+plt.grid(True, ls='--')
+plt.show()
+
+take_2.plot_maneuver_duration(-20, 20)
+take_2.plot_operations('arm_position', ('ma_filter_5', 'derivative_1'), (False, False, True), color='darkblue', label='arm_Z', showplot=False)
+take_2.plot_operations('arm_position', ('ma_filter_5', 'derivative_1'), (False, True, False), color='darkorange', label='arm_Y', showplot=False)
+take_2.plot_operations('arm_position', ('ma_filter_5', 'derivative_1'), (True, False, False), color='purple', label='arm_X', showplot=False)
+plt.legend(fontsize=20)
+plt.xlabel('Time [s]', fontsize=15)
+plt.ylabel('Velocity [mm/s]', fontsize=15)
+plt.grid(True, ls='--')
+plt.show()
+
+take_3.plot_maneuver_duration(-20, 20)
+take_3.plot_operations('arm_position', ('ma_filter_5', 'derivative_1'), (False, False, True), color='darkblue', label='arm_Z', showplot=False)
+take_3.plot_operations('arm_position', ('ma_filter_5', 'derivative_1'), (False, True, False), color='darkorange', label='arm_Y', showplot=False)
+take_3.plot_operations('arm_position', ('ma_filter_5', 'derivative_1'), (True, False, False), color='purple', label='arm_X', showplot=False)
+plt.legend(fontsize=20)
+plt.xlabel('Time [s]', fontsize=15)
+plt.ylabel('Velocity [mm/s]', fontsize=15)
+plt.grid(True, ls='--')
+plt.show()
+
+take_4.plot_maneuver_duration(-20, 20)
+take_4.plot_operations('arm_position', ('ma_filter_5', 'derivative_1'), (False, False, True), color='darkblue', label='arm_Z', showplot=False)
+take_4.plot_operations('arm_position', ('ma_filter_5', 'derivative_1'), (False, True, False), color='darkorange', label='arm_Y', showplot=False)
+take_4.plot_operations('arm_position', ('ma_filter_5', 'derivative_1'), (True, False, False), color='purple', label='arm_X', showplot=False)
+plt.legend(fontsize=20)
+plt.xlabel('Time [s]', fontsize=15)
+plt.ylabel('Velocity [mm/s]', fontsize=15)
+plt.grid(True, ls='--')
+plt.show()
